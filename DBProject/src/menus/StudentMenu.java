a488c6bbee996f74035ee3b5bb3e9f7facf18f1a
andreviloria <aviloria@calpoly.edu>
2019-06-05 15:47:11 -0700

    Update StudentMenu.java

--- DBProject/src/menus/StudentMenu.java
+++ DBProject/src/menus/StudentMenu.java
@@ -8,10 +8,16 @@ public class StudentMenu {
         int operation, ID;
         Scanner scan = new Scanner(System.in);
         
+        private Statement statement = null;
         private ResultSet resultSet = null;
-        private PreparedStatement prepStatement = null;
+        private 
+            
+            
+            aredStatement prepStatement = null;
+        String entry;
         
         //scan for first and last name
+        //needs a student object
         System.out.print("Enter your student ID: ");
         ID = scan.nextInt();
 
@@ -37,7 +43,58 @@ public class StudentMenu {
             } while (operation < 1 || operation > 6);
 
             if (operation == 1) {
-                System.out.println("Checking out.");
+                System.out.println("Checking out.")
+                statement = createStatement();
+                resultSet = statement.executeQuery("select * from Books);
+                DBTablePrinter.printResultSet(resultSet);
+
+                //loop for checkout search
+                do{
+                    System.out.print("Would you like to search by (1)serial, (2)title, (3)author, (4)genre? ");
+                    while (!scan.hasNextInt()){
+                        System.out.print("Invalid entry. Please try again: ");
+                        scan.next();
+                    }
+                    operation = scan.nextInt();
+                } while (operation < 1 || operation > 4);
+
+                //needs a loop if set returns empty
+                if (operation == 1){
+                    System.out.print("Enter the serial number: ");
+                }
+                else if (operation == 2){
+                    System.out.print("Enter the title: ");
+                }
+                else if (operation == 3){
+                    System.out.print("Enter the author: ");
+                }
+                else if (operation == 4){
+                    System.out.print("Enter the genre: ");
+                }
+                entry = scan.nextLine();
+                //skeleton set up. Needs to be either distributed in each operation choice
+                //or made into a general statement with variables
+                prepStatement = connect.prepareStatement("select * from Books where ? = ?");
+                prepStatement.setString(1, operation);
+                prepStatement.setString(2, entry);
+                resultSet = prepStatement.executeQuery();
+                
+                //if resultSet not empty
+                System.out.print("Enter the serial of the book you want to reserve or type 0 to go back: ");
+                entry = scan.nextLine();
+                //if entry == 0, loop
+                //else, check if stock == 0
+                prepStatement = connect.
+                    prepareStatement("insert into Checkout (serial, studentID, checkoutDate, dueDate) values (?, ?, ?, ?));
+                prepStatement.setString(1, entry);
+                //needs studentID from student object
+                prepStatement.setInt(2, studentID);
+                //CURDATE() used in mysql not java.sql
+                prepStatement.setString(3, CURDATE());
+                prepStatement.setString(4, CURDATE());
+                prepStatement.executeUpdate();
+                //triggers should take care to update Books.stock
+                
             }
             else if (operation == 2) {
                 System.out.println("Returning.");
