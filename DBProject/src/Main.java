42ae472f057e1ee3785ed4718e4c1802d583b178
andreviloria <aviloria@calpoly.edu>
2019-06-04 17:55:27 -0700

    Update Main.java

--- DBProject/src/Main.java
+++ DBProject/src/Main.java
@@ -8,7 +8,32 @@ public class Main {
 		Integer studentID;
 		String firstName, lastName;
 
+		int authority, status;
+		Scanner scan = new Scanner(System.in);
+		
+		do{
+			System.out.println("Are you a (1)Student, (2)Librarian, (3)Manager?");
+			while (!scan.hasNextInt()){
+				System.out.println("Invalid entry. Please try again.");
+				scan.next();
+			} 
+			authority = scan.nextLine();
+		} while (authority < 1 || authority > 3);
+		
+		if (authority == 1){
+			continue;
+		}
+		
+		if (authority == 2){
+			continue;
+		}
 
+		if (authority == 3){
+			continue;
+		}
+
+		
+		
 	}
 
 }
