a8f71ae2e74e402cce47a92888672b8b224b74a5
Nathan Tu <nathanhuytu@gmail.com>
2019-06-04 17:46:11 -0700

    Took main out of package

--- DBProject/src/Main.java
+++ DBProject/src/Main.java
@@ -1,20 +1,14 @@
-package main;
-
 import util.*;
 
 public class Main {
 
-	
+
 	public static void main(String[] args) {
 		DBConnector dbc = new DBConnector();
 		Integer studentID;
 		String firstName, lastName;
-		
-		while (true) {			
-			promptCommand();
-		}
-	}
-	
-	public 
-}
 
+
+	}
+
+}
